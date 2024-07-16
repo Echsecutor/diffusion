ef9468430289e713905cdbc35ba19eecf1a33032
Sebastian Schmittner <sebastian.schmittner@eecc.de>
2024-07-16 23:05:13 +0200

    params handling improved. added argparse to read params form json
    Signed-off-by: Sebastian Schmittner <sebastian.schmittner@eecc.de>

--- stable_diffusion.py
+++ stable_diffusion.py
@@ -3,6 +3,9 @@
 import os
 import time
 import matplotlib.pyplot as plt
+import json 
+from argparse import ArgumentParser
+
 
 from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 import torch
@@ -10,10 +13,11 @@ import torch
 default_model_id = "prompthero/openjourney-v4"
 default_guidance_scale = 12
 default_num_inference_steps = 20
+default_negative_prompt= "ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs, disfigured, deformed, body out of frame, blurry, bad anatomy, blurred, watermark, grainy, signature, cut off, draft"
+
 device = "cpu"  # Expected one of cpu, cuda, ipu, xpu, mkldnn, opengl, opencl, ideep, hip, ve, fpga, ort, xla, lazy, vulkan, mps, meta, hpu, mtia, privateuseone device type at start of device string: rocm
 
-out_path = "./"
-seeds = []
+parameters=dict()
 
 
 def callback(pipe, step_index, timestep, callback_kwargs):
@@ -26,29 +30,88 @@ def callback(pipe, step_index, timestep, callback_kwargs):
             latents / pipe.vae.config.scaling_factor, return_dict=False)[0]
         images = pipe.image_processor.postprocess(images, output_type='pil')
 
-        fig, axs = plt.subplots(len(images), 1)
+        fig, axs = plt.subplots(1, len(images), figsize=(30, 30))
         fig.suptitle(f'Images generated at step_index = {step_index}, timestep = {timestep}')
 
         for i in range(len(images)):
             axs[i].imshow(images[i])
-            axs[i].set_title(f'Seed {seeds[i]}')
+            axs[i].set_title(f'Seed {parameters["seeds"][i]}')
         
-        plt.show()
+        plt.tight_layout()
+        #plt.show()
 
-        plt.savefig(f'{out_path}/tmp_{{seeds[i]}}_{timestep}.png')
+        plt.savefig(f'{parameters['out_path']}/step_index_{step_index}.png')
+        
+        print(f"callback_kwargs={callback_kwargs}")
 
     return callback_kwargs
 
+def parameters_to_pipeline_args(parameters):
+    return {'num_inference_steps': parameters['num_inference_steps'],
+            'guidance_scale': parameters['guidance_scale'],
+            'generator': [torch.Generator(device).manual_seed(i) for i in parameters['seeds']],
+            'negative_prompt': len(parameters['seeds']) * [parameters['negative_prompt']],
+            'prompt': len(parameters['seeds']) * [parameters['prompt']]
+            }
 
-def main():
-    global out_path, seeds
-    model_id = input(f"model [{default_model_id}]: ")
-    if not model_id:
-        model_id = default_model_id
-    print(f"Loading Model {model_id}")
+def get_parameters():
+    global parameters
+    
+    parser = ArgumentParser( prog='Exes diffusion CI', description='Run a stable diffusion pipeline to generate images')
+    parser.add_argument("-p", "--parameters", help="Read parameters from json FILE instead of querying interactively", metavar="FILE")
+    
+    args = parser.parse_args()
+    
+    if args.parameters:
+        with open(args.parameters) as parameter_file:
+            parameters = json.load(parameter_file)
+        return
+    
+    parameters['model_id'] = input(f"model [{default_model_id}]: ")
+    if not parameters['model_id']:
+        parameters['model_id'] = default_model_id
+    
+    parameters['prompt'] = input("prompt: ")
+    parameters['negative_prompt'] = input(f"negative_prompt [{default_negative_prompt}]: ")
+    if not parameters['negative_prompt']:
+        parameters['negative_prompt']=default_negative_prompt
+    
+    parameters['num_inference_steps'] = input(
+        f"num_inference_steps [{default_num_inference_steps}]: ")
+    if not parameters['num_inference_steps']:
+        parameters['num_inference_steps'] = default_num_inference_steps
+    parameters['num_inference_steps'] = int(parameters['num_inference_steps'])
+
+    seed = "-1"
+    parameters['seeds']=[]
+
+    print("Enter the seeds for the images to be generated.")
+    print("The number of seeds equals the number of generated images.")
+    print("Press enter when no more seeds should be added.")
+    while seed != "":
+        seed = input("Seed: ")
+        if seed != "":
+            parameters['seeds'].append(int(seed))
+
+    parameters['guidance_scale'] = input(
+        f"guidance_scale (1-20) [{default_guidance_scale}]: ")
+    if not parameters['guidance_scale']:
+        parameters['guidance_scale'] = default_guidance_scale
+    parameters['guidance_scale'] = float(parameters['guidance_scale'])
+    parameters['out_path']=""
+
+def mk_out_dir():
+    global parameters
+    
+    if not parameters['out_path'] or os.path.exists(parameters['out_path']):
+        parameters['out_path'] = f"./{int(time.time())}"
+    os.makedirs(parameters['out_path'])
+
+def load_model():
+    print(f"Loading Model {parameters['model_id']}")
 
     pipeline = StableDiffusionPipeline.from_pretrained(
-        model_id,
+        parameters['model_id'],
         use_safetensors=True,
         safety_checker=None,
         requires_safety_checker=False
@@ -59,48 +122,27 @@ def main():
 
     pipeline = pipeline.to(device)
 
-    prompt = input("prompt: ")
-    negative_prompt = input("negative_prompt: ")
-    num_inference_steps = input(
-        f"num_inference_steps [{default_num_inference_steps}]: ")
-    if not num_inference_steps:
-        num_inference_steps = default_num_inference_steps
-    num_inference_steps = int(num_inference_steps)
 
-    seed = "-1"
+def main():
+    
+    get_parameters()
+    
+    mk_out_dir()
+    
+    with open(f'{parameters['out_path']}/parameters.json', 'w') as params_file: 
+        params_file.write(json.dumps(parameters))
+    
+    load_model()
 
-    print("Enter the seeds for the images to be generated.")
-    print("The number of seeds equals the number of generated images.")
-    print("Press enter when no more seeds should be added.")
-    while seed != "":
-        seed = input("Seed: ")
-        if seed != "":
-            seeds.append(int(seed))
-
-    guidance_scale = input(
-        f"guidance_scale (1-20) [{default_guidance_scale}]: ")
-    if not guidance_scale:
-        guidance_scale = default_guidance_scale
-    guidance_scale = float(guidance_scale)
-
-    out_path = f"./{int(time.time())}"
-    os.makedirs(out_path)
-
-    print(f"Generating Images")
-    images = pipeline(callback_on_step_end=callback,
-                      num_inference_steps=num_inference_steps,
-                      guidance_scale=guidance_scale,
-                      generator=[torch.Generator(
-                          device).manual_seed(i) for i in seeds],
-                      negative_prompt=len(seeds) * [negative_prompt],
-                      prompt=len(seeds) * [prompt]
-                      ).images
+    print(f"Generating images in {parameters['out_path']} with parameters: {parameters}")
+    
+    images = pipeline(callback_on_step_end=callback, **parameters_to_pipeline_args(parameters)).images
     i = 0
     for image in images:
-        image.save(f"{out_path}/{seeds[i]}.png")
+        image.save(f"{parameters['out_path']}/{parameters['seeds'][i]}.png")
         i += 1
 
-    print(f"Saved output images to {out_path}")
+    print(f"Saved output images to {parameters['out_path']}")
 
 
 if __name__ == "__main__":
